847d7d9b3afaea73e0795690c7ac38cf39aaedfd
Empire Community <b7ightnova@gmail.com>
2024-12-15 16:19:34 +0100

    Add files via upload

--- commands/📈 Ranking/rank.js
+++ commands/📈 Ranking/rank.js
@@ -3,13 +3,13 @@ const config = require(`${process.cwd()}/botconfig/config.json`)
 var ee = require(`${process.cwd()}/botconfig/embed.json`)
 module.exports = {
 	//definition
-	name: "addlevel", //the name of the command 
+	name: "rank", //the name of the command 
 	category: "📈 Ranking", //the category this will be listed at, for the help cmd
-	aliases: [""], //every parameter can be an alias
+	aliases: ["textrank", "chatrank"], //every parameter can be an alias
 	cooldown: 4, //this will set it to a 4 second cooldown
-	usage: "addlevel <@User> <Amount>", //this is for the help command for EACH cmd
-  	description: "Add a specific amount of Levels to a User", //the description of the command
-	type: "manage",
+	usage: "rank [@User]", //this is for the help command for EACH cmd
+  	description: "Shows the Rank of a User", //the description of the command
+	type: "info",
 	//running the command with the parameters: client, message, args, user, text, prefix
   	run: async (client, message, args, cmduser, text, prefix) => {
     
--- commands/📈 Ranking/voiceleaderboard.js
+++ commands/📈 Ranking/voiceleaderboard.js
@@ -3,15 +3,15 @@ const config = require(`${process.cwd()}/botconfig/config.json`)
 var ee = require(`${process.cwd()}/botconfig/embed.json`)
 module.exports = {
 	//definition
-	name: "addlevel", //the name of the command 
+	name: "voiceleaderboard", //the name of the command 
 	category: "📈 Ranking", //the category this will be listed at, for the help cmd
-	aliases: [""], //every parameter can be an alias
+	aliases: ["voicelb", "voicetop"], //every parameter can be an alias
 	cooldown: 4, //this will set it to a 4 second cooldown
-	usage: "addlevel <@User> <Amount>", //this is for the help command for EACH cmd
-  	description: "Add a specific amount of Levels to a User", //the description of the command
-	type: "manage",
+	usage: "voiceleaderboard", //this is for the help command for EACH cmd
+  	description: "Shows the Top 10 Voice-Leaderboard", //the description of the command
+	  type: "info",
 	//running the command with the parameters: client, message, args, user, text, prefix
-  	run: async (client, message, args, cmduser, text, prefix) => {
+  run: async (client, message, args, cmduser, text, prefix) => {
     
 
 	}
